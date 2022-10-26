e8b2756a04967f10364490ff1528e0994af107d9
delcarlswlhs <113708789+delcarlswlhs@users.noreply.github.com>
2022-10-26 08:44:27 -0700

    moved work to a backup repository

---
+++ CodeHSU2L1/GrilledCheese.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ * The GrilledCheese class describes a grilled cheese sandwich.
+ * This file states what every GrilledCheese object will have, but
+ * this file is NOT an object.
+ * 
+ * Every grilled cheese needs a type of bread, a type of cheese,
+ * what temperature to cook the grilled cheese (should it be burned!?),
+ * and if the sandwich should have twice as much cheese as normal.
+ */ 
+
+public class GrilledCheese
+{
+    
+    // Attributes of all Grilled Cheese objects
+    private String cheeseType;
+    private String breadType;
+    private double volume;
+    private int temp;
+    private boolean isBurned;
+    private boolean grilled;
+    private boolean hasMeat;
+    private boolean hasBread;
+    private int numSlicesBread;
+    private int numSlicesCheese;
+    
+    // The methods of the class will go here
+ 
+    // Since this class does not have a main method,
+    // nothing will happen when you run it.
+   
+}
---
+++ CodeHSU2L1/RectangleTester.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class RectangleTester here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class RectangleTester
+{
+    public static void main(String[] args){
+        
+        Rectangle rect1 = new Rectangle(7.3, 5.1);
+        System.out.println(rect1);
+        
+        double myRectArea = rect1.getArea();
+        System.out.println(myRectArea);
+    }
+}
